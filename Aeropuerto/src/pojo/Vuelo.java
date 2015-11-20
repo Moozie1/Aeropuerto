ba65be45a173ce422f1d150a028cfee019513d09
angelnuneztorron <angelnuneztorron@172.25.16.134>
2015-11-20 09:01:11 +0100

    Hecho pojo vuelo

--- Aeropuerto/src/pojo/Vuelo.java
+++ Aeropuerto/src/pojo/Vuelo.java
@@ -1,5 +1,92 @@
 package pojo;
 
 public class Vuelo {
-
+	
+	private int id;
+	private Avion avion;
+	private Terminal terminal;
+	
+	
+	public Vuelo(){
+		
+	}
+	public Vuelo(int id, Avion avion, Terminal terminal){
+		this.id=id;
+		this.avion=avion;
+		this.terminal=terminal;
+	}
+	
+	public Vuelo(Avion avion, Terminal terminal){
+		this.avion=avion;
+		this.terminal=terminal;
+	}
+	public int getId(){
+		return id;
+	}
+	public Avion getAvion(){
+		return avion;
+	}
+	public Terminal getTerminal(){
+		return terminal;
+	}
+	public void setAvion(Avion avion){
+		this.avion=avion;
+	}
+	public void setId(int id){
+		this.id=id;
+	}
+	public void setTerminal(Terminal terminal){
+		this.terminal=terminal;
+	}
+	
+	public String toString(){
+		String a="Id: "+id+"Vuelo: "+avion+"Terminal: "+terminal;
+		return a;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((avion == null) ? 0 : avion.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((terminal == null) ? 0 : terminal.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vuelo other = (Vuelo) obj;
+		if (avion == null) {
+			if (other.avion != null)
+				return false;
+		} else if (!avion.equals(other.avion))
+			return false;
+		if (id != other.id)
+			return false;
+		if (terminal == null) {
+			if (other.terminal != null)
+				return false;
+		} else if (!terminal.equals(other.terminal))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
